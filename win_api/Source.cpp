67b1487adfa1f98e993299e3ce5811bc0bda2006
cloudadmin <jonathanhrtntt@gmail.com>
2018-08-21 16:36:14 +0100

    changes made to source

--- win_api/Source.cpp
+++ win_api/Source.cpp
@@ -3,25 +3,30 @@
 #include <windows.h>
 #include <atlbase.h>
 #include <winternl.h>
+#include <ntstatus.h>
 #include <cstdio>
 #include <cstdlib>
 #pragma comment(lib, "ntdll")
 
+#include <system_error>
+
 #ifndef FILE_CS_FLAG_CASE_SENSITIVE_DIR
+
 #define FileCaseSensitiveInformation (FILE_INFORMATION_CLASS)71
 #define FILE_CS_FLAG_CASE_SENSITIVE_DIR 0x00000001
+
 typedef struct {
 	ULONG Flags;
 } FILE_CASE_SENSITIVE_INFORMATION, *PFILE_CASE_SENSITIVE_INFORMATION;
+
 #endif
 
-EXTERN_C NTSTATUS NTSYSAPI NTAPI NtSetInformationFile(
+extern "C" NTSTATUS NTSYSAPI NTAPI NtSetInformationFile(
 	_In_  HANDLE                 FileHandle,
 	_Out_ PIO_STATUS_BLOCK       IoStatusBlock,
 	_In_  PVOID                  FileInformation,
 	_In_  ULONG                  Length,
-	_In_  FILE_INFORMATION_CLASS FileInformationClass
-);
+	_In_  FILE_INFORMATION_CLASS FileInformationClass);
 
 int __cdecl wmain(int, PWSTR argv[])
 {
@@ -32,13 +37,16 @@ int __cdecl wmain(int, PWSTR argv[])
 		d.Detach();
 		return EXIT_FAILURE;
 	}
-	int val = FILE_CS_FLAG_CASE_SENSITIVE_DIR & ~FILE_CS_FLAG_CASE_SENSITIVE_DIR;
+	//int val = FILE_CS_FLAG_CASE_SENSITIVE_DIR & ~FILE_CS_FLAG_CASE_SENSITIVE_DIR;
 	IO_STATUS_BLOCK iob;
-	FILE_CASE_SENSITIVE_INFORMATION file_cs = { val };
+	FILE_CASE_SENSITIVE_INFORMATION file_cs = { 0 };// FILE_CS_FLAG_CASE_SENSITIVE_DIR
+
 	NTSTATUS status = NtSetInformationFile(d, &iob, &file_cs, sizeof file_cs, FileCaseSensitiveInformation);
 	if (NT_ERROR(status))
 	{
-		printf("NtSetInformationFile %08lx\n", status);
+		const auto err = ::RtlNtStatusToDosError(status);
+
+		printf("NtSetInformationFile failed: %s\n", std::system_category().message(err).c_str());
 		return EXIT_FAILURE;
 	}
 }
